0d2c690520035f21e852248937191b93382a33d9
TekCode <est.felipe.aponte@unimilitar.edu.co>
2025-10-18 18:39:58 -0500

    Added SMOTE for data balancing

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from src.data_preprocessing import load_and_preprocess
+from src.data_preprocessing import load_and_preprocess, apply_smote
 from src.train_model import train_model
 from src.evaluate_model import evaluate_model
 from src.export_model import save_model
@@ -8,28 +8,35 @@ from pathlib import Path
 import mlflow
 import mlflow.sklearn
 
-BASE_DIR = Path(__file__).resolve().parent.parent  # sube un nivel desde scripts/
+BASE_DIR = Path(__file__).resolve().parent.parent
 TRAIN_DATA_PATH = BASE_DIR / "data" / "processed" / "train_data.csv"
 
-# --- Generar feature 'is_anomaly' ---
-fraud_data =  train_isolation_forest(TRAIN_DATA_PATH)
+# Config
+USE_SMOTE = True
+SMOTE_STRATEGY = 0.5
 
-# --- Cargar y preprocesar ---
+# Pipeline
+fraud_data = train_isolation_forest(TRAIN_DATA_PATH)
 X_scaled, y, scaler = load_and_preprocess(fraud_data)
 
-# --- Train/Test split ---
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.3, stratify=y, random_state=42
 )
 
-# --- Hiperparámetros encontrados ---
+# Aplicar SMOTE
+if USE_SMOTE:
+    X_train, y_train = apply_smote(X_train, y_train, sampling_strategy=SMOTE_STRATEGY)
+    scale_pos_weight = 1
+else:
+    scale_pos_weight = (y_train == 0).sum() / (y_train == 1).sum()
+
 best_params = {
     'colsample_bytree': 0.8731,
     'learning_rate': 0.1161,
     'max_depth': 10,
     'min_child_weight': 1,
     'n_estimators': 370,
-    'scale_pos_weight': 578.5465,
+    'scale_pos_weight': scale_pos_weight,
     'subsample': 0.6,
     'random_state': 42,
     'use_label_encoder': False,
@@ -40,22 +47,19 @@ mlflow.set_experiment("Fraud Detection")
 mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 
 with mlflow.start_run():
-    # --- Entrenar ---
-    model = train_model(X_train, y_train, best_params)
+    mlflow.log_param("use_smote", USE_SMOTE)
     mlflow.log_params(best_params)
-
-    # --- Evaluar ---
+    
+    model = train_model(X_train, y_train, best_params)
     metrics = evaluate_model(model, X_test, y_test)
-    print("=== Final Model Evaluation ===")
+    
     for k, v in metrics.items():
-        print(f"{k}: {v:.4f}")
         mlflow.log_metric(k, v)
-
-    # --- Exportar ---
+    
     save_model(model, 'fraud_model.pkl')
-    print("\n✅ Pipeline completo: Modelo y scaler exportados.")
-
-     # --- Log scaler ---
+    
     mlflow.log_artifact(BASE_DIR / "models" / "scaler.pkl")
     mlflow.log_artifact(BASE_DIR / "models" / "iso.pkl")
     mlflow.sklearn.log_model(model, artifact_path="fraud_model")
+
+print("✅ Pipeline completo")
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import joblib
 from pathlib import Path
+from imblearn.over_sampling import SMOTE
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,3 +20,15 @@ def load_and_preprocess(data):
     joblib.dump(scaler, BASE_DIR / "models" / 'scaler.pkl')
     
     return X_scaled, y, scaler
+
+def apply_smote(X_train, y_train, sampling_strategy='auto', random_state=42):    
+    # Aplicar SMOTE
+    smote = SMOTE(
+        sampling_strategy=sampling_strategy,
+        random_state=random_state,
+        k_neighbors=5
+    )
+    
+    X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
+    
+    return X_resampled, y_resampled
